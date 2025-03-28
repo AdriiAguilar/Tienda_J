83ca10cd8fa324b5e6fdc8116e3d1d582b4e6ed6
AdriiAguilar <127550346+AdriiAguilar@users.noreply.github.com>
2025-03-27 22:28:20 -0600

    Update Categoria.java

--- src/main/java/com/tienda/domain/Categoria.java
+++ src/main/java/com/tienda/domain/Categoria.java
@@ -4,15 +4,15 @@ import java.io.Serializable;
 import lombok.Data;
 @Data
 @Entity
-@Table (name="categoria")
+@Table(name="categoria")
 public class Categoria implements Serializable {
     private static final long serialVersionUID = 1L;
     
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column (name="id_categoria")
+    @Column(name="id_categoria")
     private Long idCategoria;
-    private String description;
+    private String descripcion;
     private String rutaImagen;
     private boolean activo;
     
