3e24c01fe47031b516cf7fdd73a7bfdfc9becd00
AdriiAguilar <127550346+AdriiAguilar@users.noreply.github.com>
2025-04-03 17:45:27 -0600

    Practica#4
    Consultas ampliadas

--- src/main/java/com/tienda/dao/CategoriaDao.java
+++ src/main/java/com/tienda/dao/CategoriaDao.java
@@ -2,7 +2,13 @@ package com.tienda.dao;
 
 import com.tienda.domain.Categoria;
 import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.List; 
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.jpa.repository.query,Param;
 
 public interface CategoriaDao extends JpaRepository <Categoria, Long> {
+
+    public List<Producto> findByPrecioBetweenOrderByDescripcion(double 
+precioInf, double precioSup); 
 
 }
